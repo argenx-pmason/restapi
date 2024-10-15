db982eb37d9e319b33a42fd1a7111b18e74f2bac
pmason <pmason@argenx.com>
2024-10-15 13:34:13 +0100

    auto

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import "./App.css";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import {
   Box,
-  Grid,
   Button,
   Dialog,
   DialogTitle,
@@ -12,39 +11,26 @@ import {
   Toolbar,
   TextField,
   IconButton,
+  Tabs,
+  Tab,
 } from "@mui/material";
-// import { DataGridPro, GridToolbar } from "@mui/x-data-grid-pro";
 import { Info } from "@mui/icons-material";
 import convert from "xml-js";
-import { LicenseInfo } from "@mui/x-license-pro";
-import { getDir, xmlToJson } from "./utility";
+import { LicenseInfo } from "@mui/x-license";
 const App = () => {
   LicenseInfo.setLicenseKey(
     "6b1cacb920025860cc06bcaf75ee7a66Tz05NDY2MixFPTE3NTMyNTMxMDQwMDAsUz1wcm8sTE09c3Vic2NyaXB0aW9uLEtWPTI="
   );
   const title = "LSAF REST API testing",
+    { host } = window.location,
+    realhost = host.includes("sharepoint") ? "xarprod.ondemand.sas.com" : host,
+    api = "https://" + realhost + "/lsaf/api",
     innerHeight = window.innerHeight,
-    urlPrefix = window.location.protocol + "//" + window.location.host,
-    { href } = window.location,
-    mode = href.startsWith("http://localhost") ? "local" : "remote",
-    server = href.split("//")[1].split("/")[0],
-    // webDavPrefix = urlPrefix + "/lsaf/webdav/repo",
-    // fileViewerPrefix = `https://${server}/lsaf/filedownload/sdd:/general/biostat/tools/fileviewer/index.html?file=`,
     [openInfo, setOpenInfo] = useState(false),
     [token, setToken] = useState(null),
     [encryptedPassword, setEncryptedPassword] = useState(null),
-    [rows, setRows] = useState([
-      { id: 1, name: "John", age: 30, city: "New York" },
-      { id: 2, name: "Jane", age: 25, city: "Chicago" },
-    ]),
-    [cols, setCols] = useState([
-      { field: "id", headerName: "ID", width: 100 },
-      { field: "name", headerName: "Name", width: 200 },
-      { field: "age", headerName: "Age", width: 100 },
-      { field: "city", headerName: "City", width: 200 },
-    ]),
     encryptPassword = () => {
-      const url = "https://xarprod.ondemand.sas.com/lsaf/api/encrypt",
+      const url = api + "/encrypt",
         myHeaders = new Headers();
       myHeaders.append(
         "Authorization",
@@ -65,10 +51,15 @@ const App = () => {
         })
         .catch((error) => console.error(error));
     },
+    [jobPath, setJobPath] = useState(
+      "/jobs/repository/general/biostat/jobs/gadam_ongoing_studies/dev/jobs/sdtm_part1.job"
+    ),
     [username, setUsername] = useState("pmason"),
     [password, setPassword] = useState(""),
+    [timeTaken, setTimeTaken] = useState(0),
+    // logon with unencrypted password
     logon = () => {
-      const url = "https://xarprod.ondemand.sas.com/lsaf/api/logon",
+      const url = api + "/logon",
         myHeaders = new Headers();
       myHeaders.append(
         "Authorization",
@@ -88,23 +79,85 @@ const App = () => {
         })
         .catch((error) => console.error(error));
     },
+    // logon with encrypted password, submit job, wait & check status
+    logon2 = async () => {
+      const url = api + "/logon",
+        myHeaders = new Headers();
+      myHeaders.append(
+        "Authorization",
+        "Basic " + btoa(username + ":" + encryptedPassword)
+      );
+      const requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        redirect: "follow",
+      };
+
+      fetch(url, requestOptions)
+        .then(async (response) => {
+          const authToken = response.headers.get("x-auth-token");
+          console.log("authToken", authToken, "response", response);
+          setToken(authToken);
+          const subId = await submitJob(authToken);
+          await waitTillJobCompletes(subId, authToken);
+          await otherSteps(subId, authToken);
+        })
+        .catch((error) => console.error(error));
+    },
+    sleep = (t) => new Promise((r) => setTimeout(r, t)),
+    waitTillJobCompletes = async (subId, tok) => {
+      // loop until job status is completed or failed or cancelled
+      let thisStatus = "";
+      // set time now
+      const startTime = new Date().getTime();
+      console.log("subId", subId, "submissionId", submissionId, "tok", tok);
+      while (thisStatus !== "COMPLETED (30300)") {
+        // console.log("submissionId", submissionId);
+        //TODO - wait till we have a sumission ID
+        await sleep(3000);
+        thisStatus = await getJobStatus(subId, tok);
+        console.log("thisStatus", thisStatus);
+        if (thisStatus === "failed" || thisStatus === "cancelled") {
+          break;
+        }
+        // work out time from start to now
+        const endTime = new Date().getTime(),
+          timeDiff = endTime - startTime,
+          // convert timediff to seconds
+          seconds = timeDiff / 1000;
+        console.log("seconds", seconds);
+        setTimeTaken(seconds);
+        if (seconds > 60) {
+          break;
+        }
+      }
+    },
+    otherSteps = async (subId, tok) => {
+      const pathMan = await getPathManifest(subId, tok);
+      console.log("pathMan", pathMan);
+      const res = await getManifest(tok, pathMan);
+      console.log("res", res);
+      getFileContents(tok, res);
+    },
+    [tabValue, setTabValue] = useState("1"),
+    handleChangeTab = (event, newValue) => {
+      setTabValue(newValue);
+    },
     [submitStatus, setSubmitStatus] = useState(null),
     [submissionId, setSubmissionId] = useState(null),
-    submitJob = () => {
+    submitJob = (passedToken) => {
       const url =
-          // "https://xarprod.ondemand.sas.com/lsaf/api//repository/files/",
-          "https://xarprod.ondemand.sas.com/lsaf/api",
-        jobPath =
-          "/jobs/repository/general/biostat/jobs/gadam_ongoing_studies/dev/jobs/sdtm_part1.job",
+          // api+"//repository/files/",
+          api,
         myHeaders = new Headers();
-      myHeaders.append("X-Auth-Token", token);
+      myHeaders.append("X-Auth-Token", passedToken || token);
       const requestOptions = {
         method: "PUT",
         headers: myHeaders,
         redirect: "follow",
       };
 
-      fetch(url + jobPath + "?action=run", requestOptions)
+      return fetch(url + jobPath + "?action=run", requestOptions)
         .then((response) => {
           console.log("response", response);
           return response.json();
@@ -113,22 +166,30 @@ const App = () => {
           console.log("responseJson", responseJson);
           setSubmitStatus(responseJson?.status);
           setSubmissionId(responseJson?.submissionId);
+          return responseJson?.submissionId;
         })
         .catch((error) => console.error(error));
     },
     [jobStatus, setJobStatus] = useState(null),
-    getJobStatus = () => {
-      const url = "https://xarprod.ondemand.sas.com/lsaf/api",
-        apiRequest = `/jobs/submissions/${submissionId}/status`,
+    getJobStatus = (subId, tok) => {
+      console.log("subId", subId, "submissionId", submissionId, "tok", tok);
+      if (!submissionId && !subId) {
+        return "no submission ID";
+      }
+      const useSubmissionId = subId || submissionId,
+        useToken = tok || token,
+        url = api,
+        apiRequest = `/jobs/submissions/${useSubmissionId}/status`,
         myHeaders = new Headers();
-      myHeaders.append("X-Auth-Token", token);
+      myHeaders.append("X-Auth-Token", useToken);
       const requestOptions = {
         method: "GET",
         headers: myHeaders,
         redirect: "follow",
       };
+      console.log("useSubmissionId", useSubmissionId, "useToken", useToken);
 
-      fetch(url + apiRequest, requestOptions)
+      return fetch(url + apiRequest, requestOptions)
         .then((response) => {
           console.log("response", response);
           return response.json();
@@ -137,22 +198,25 @@ const App = () => {
           const status = responseJson?.type + " (" + responseJson?.code + ")";
           console.log("responseJson", responseJson, "status", status);
           setJobStatus(status);
+          return status;
         })
         .catch((error) => console.error(error));
     },
     [pathManifest, setPathManifest] = useState(null),
-    getPathManifest = () => {
-      const url = "https://xarprod.ondemand.sas.com/lsaf/api",
-        apiRequest = `/jobs/submissions/${submissionId}/manifest`,
+    getPathManifest = (subId, tok) => {
+      const useSubmissionId = subId || submissionId,
+        useToken = tok || token,
+        url = api,
+        apiRequest = `/jobs/submissions/${useSubmissionId}/manifest`,
         myHeaders = new Headers();
-      myHeaders.append("X-Auth-Token", token);
+      myHeaders.append("X-Auth-Token", useToken);
       const requestOptions = {
         method: "GET",
         headers: myHeaders,
         redirect: "follow",
       };
 
-      fetch(url + apiRequest, requestOptions)
+      return fetch(url + apiRequest, requestOptions)
         .then((response) => {
           console.log("response", response);
           return response.json();
@@ -161,22 +225,26 @@ const App = () => {
           const path = responseJson?.path;
           console.log("responseJson", responseJson, "path", path);
           setPathManifest(path);
+          return path;
         })
         .catch((error) => console.error(error));
     },
     [log, setLog] = useState(null),
-    getManifest = () => {
-      const url = "https://xarprod.ondemand.sas.com/lsaf/api",
-        apiRequest = `/repository/files/${pathManifest}?component=contents`,
+    [resultFile, setResultFile] = useState(null),
+    getManifest = (tok, pathMan) => {
+      const useToken = tok || token,
+        useManifest = pathMan || pathManifest,
+        url = api,
+        apiRequest = `/repository/files/${useManifest}?component=contents`,
         myHeaders = new Headers();
-      myHeaders.append("X-Auth-Token", token);
+      myHeaders.append("X-Auth-Token", useToken);
       const requestOptions = {
         method: "GET",
         headers: myHeaders,
         redirect: "follow",
       };
 
-      fetch(url + apiRequest, requestOptions)
+      return fetch(url + apiRequest, requestOptions)
         .then((response) => {
           console.log("response", response);
           return response.text();
@@ -190,20 +258,27 @@ const App = () => {
           console.log("dataJSON", dataJSON);
           const jm = dataJSON["job-manifest"],
             { job } = jm,
-            { logs } = job,
+            { logs, results } = job,
             { log } = logs,
             rf = log["repository-file"],
-            { _text } = rf;
+            { _text } = rf,
+            { result } = results,
+            repositoryFile = result["repository-file"],
+            { _text: _resultFile } = repositoryFile;
           setLog(_text);
+          setResultFile(_resultFile);
+          return _resultFile;
         })
         .catch((error) => console.error(error));
     },
     [fileContents, setFileContents] = useState(null),
-    getFileContents = () => {
-      const url = "https://xarprod.ondemand.sas.com/lsaf/api",
-        apiRequest = `/repository/files/${log}?component=contents`,
+    getFileContents = (tok, file) => {
+      const useToken = tok || token,
+        useFile = file || log,
+        url = api,
+        apiRequest = `/repository/files/${useFile}?component=contents`,
         myHeaders = new Headers();
-      myHeaders.append("X-Auth-Token", token);
+      myHeaders.append("X-Auth-Token", useToken);
       const requestOptions = {
         method: "GET",
         headers: myHeaders,
@@ -240,6 +315,10 @@ const App = () => {
           >
             &nbsp;&nbsp;{title}&nbsp;&nbsp;
           </Box>
+          <Tabs value={tabValue} onChange={handleChangeTab}>
+            <Tab label="Step by step" value="1" />
+            <Tab label="Automated" value="2" />
+          </Tabs>
           <Box sx={{ flexGrow: 1 }}></Box>
           <Tooltip title="Information about this screen">
             <IconButton
@@ -254,34 +333,98 @@ const App = () => {
           </Tooltip>
         </Toolbar>
       </AppBar>
-      <Grid sx={{ mt: 8 }} container>
-        <Grid item xs={12}>
-          <TextField
-            label="Username"
-            value={username}
-            onChange={(e) => {
-              setUsername(e.target.value);
-            }}
-          />
-          <TextField
-            label="Password"
-            value={password}
-            type="password"
-            onChange={(e) => {
-              setPassword(e.target.value);
-            }}
-          />
-          <br />
-          <Button onClick={() => encryptPassword()}>
-            Get Encrypted Password
-          </Button>
-          <Box sx={{ backgroundColor: "#f7f7f7" }}>
-            Encrypted Password: {encryptedPassword}
+      <Box sx={{ mt: 9 }} hidden={tabValue !== "1"}>
+        <TextField
+          label="Username"
+          value={username}
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        />
+        <TextField
+          label="Password"
+          value={password}
+          type="password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        <br />
+        <Button onClick={() => encryptPassword()}>
+          Get Encrypted Password
+        </Button>
+        <Box sx={{ backgroundColor: "#e6f2ff" }}>
+          Encrypted Password: {encryptedPassword}
+        </Box>
+        <Button onClick={() => logon()}>Logon & get token</Button>
+        <Box sx={{ backgroundColor: "#f7f7f7" }}>Token: {token}</Box>
+        <TextField
+          label="Path to Job"
+          value={jobPath}
+          onChange={(e) => {
+            setJobPath(e.target.value);
+          }}
+          sx={{ mt:3, width: 800 }}
+        />
+        <Button onClick={() => submitJob()}>Submit job</Button>
+        <Box sx={{ backgroundColor: "#e6f2ff" }}>
+          Status:{" "}
+          <b>
+            {submitStatus
+              ? submitStatus.type + " (" + submitStatus.code + ")"
+              : null}
+          </b>
+        </Box>
+        <Box sx={{ backgroundColor: "#f7f7f7" }}>
+          Submission ID: {submissionId || null}
+        </Box>
+        <Button onClick={() => getJobStatus()}>Check job status</Button>
+        <Box sx={{ backgroundColor: "#e6f2ff" }}>
+          Job status: <b>{jobStatus || null}</b>
+        </Box>
+        <Button onClick={() => getPathManifest()}>Get path to manifest</Button>
+        <Box sx={{ backgroundColor: "#f7f7f7" }}>
+          Manifest path: <b>{pathManifest || null}</b>
+        </Box>
+        <Button onClick={() => getManifest()}>Get manifest file & extract path to SAS log</Button>
+        <Box sx={{ backgroundColor: "#e6f2ff" }}>
+          Log path extracted from manifest XML: <code>{log || null}</code>
+        </Box>
+        <Button onClick={() => getFileContents()}>
+          Get contents of Log file
+        </Button>
+        <Box sx={{ backgroundColor: "#f7f7f7" }}>
+          Log: <code>{fileContents || null}</code>
+        </Box>
+      </Box>
+      <Box sx={{ mt: 9 }} hidden={tabValue !== "2"}>
+        <Box sx={{ height: innerHeight - 50, width: "100%" }}>
+          <Box sx={{ backgroundColor: "#e6f2ff" }}>
+            Using username: <b>{username}</b>
+            <br />
+            Using encrypted Password: <b>{encryptedPassword}</b>
           </Box>
-          <Button onClick={() => logon()}>Logon & get token</Button>
+          <TextField
+            label="Path to Job"
+            value={jobPath}
+            onChange={(e) => {
+              setJobPath(e.target.value);
+            }}
+            sx={{ mt:2,  width: 800 }}
+          />
+          <ul>
+            <li>Logon with encrypted password</li>
+            <li>Get token</li>
+            <li>Submit job</li>
+            <li>Wait 3 secs</li>
+            <li>Check status until complete</li>
+            <li>Get manifest</li>
+            <li>Get path to results</li>
+            <li>Get results</li>
+          </ul>
+          <Button onClick={() => logon2()}>Go</Button>
           <Box sx={{ backgroundColor: "#f7f7f7" }}>Token: {token}</Box>
-          <Button onClick={() => submitJob()}>Submit job</Button>
-          <Box sx={{ backgroundColor: "#f7f7f7" }}>
+          <Box sx={{ backgroundColor: "#e6f2ff" }}>
             Status:{" "}
             <b>
               {submitStatus
@@ -290,45 +433,29 @@ const App = () => {
             </b>
           </Box>
           <Box sx={{ backgroundColor: "#f7f7f7" }}>
-            Submission ID: {submissionId ? submissionId : null}
+            Submission ID: {submissionId || null}
           </Box>
-          <Button onClick={() => getJobStatus()}>Check job status</Button>
-          <Box sx={{ backgroundColor: "#f7f7f7" }}>
-            Job status: <b>{jobStatus ? jobStatus : null}</b>
+          <Box sx={{ backgroundColor: "#e6f2ff" }}>
+            Job status: <b>{jobStatus || null}</b>
+            <br />
+            Time taken: <b>{timeTaken || null}</b>
           </Box>
-          <Button onClick={() => getPathManifest()}>
-            Get path to manifest
-          </Button>
           <Box sx={{ backgroundColor: "#f7f7f7" }}>
-            Manifest path: <b>{pathManifest ? pathManifest : null}</b>
+            Manifest path: <b>{pathManifest || null}</b>
           </Box>
-          <Button onClick={() => getManifest()}>Get manifest file</Button>
-          <Box sx={{ backgroundColor: "#f7f7f7" }}>
+          <Box sx={{ backgroundColor: "#e6f2ff" }}>
             Log path extracted from manifest XML:{" "}
             <code>{log ? log : null}</code>
+            <br />
+            Path to results extracted from manifest XML:{" "}
+            <code>{resultFile ? resultFile : null}</code>
           </Box>
-          <Button onClick={() => getFileContents()}>
-            Get contents of Log file
-          </Button>
-          <Box sx={{ backgroundColor: "#f7f7f7" }}>
-            Log: <code>{fileContents ? fileContents : null}</code>
+          <Box sx={{ backgroundColor: "#ffffe6" }}>
+            <br />
+            <code>{fileContents || null}</code>
           </Box>
-          {/*   <Box sx={{ height: innerHeight - 50, width: "100%" }}>
-            <DataGridPro
-              autoHeight={true}
-              rows={rows}
-              columns={cols}
-              slots={{ toolbar: GridToolbar }}
-              slotProps={{
-                toolbar: {
-                  showQuickFilter: true,
-                },
-              }}
-              // sx={{ "& .MuiDataGrid-row": { fontSize: fontSize } }}
-            />
-          </Box>*/}
-        </Grid>
-      </Grid>
+        </Box>
+      </Box>
       {/* Dialog with General info about this screen */}
       <Dialog
         fullWidth
